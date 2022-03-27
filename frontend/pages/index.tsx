0f4cff5319ca75d85f8f8df990d71eceb434d484
tulakann <tulakan.r@gmail.com>
2022-03-27 20:51:01 +0700

    feat: implement dropzone

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from "next";
 import Layout from "../components/layout";
+import Dropzone from "../components/dropzone";
+import { Flex } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
-  return <Layout>hello</Layout>;
+  return (
+    <Layout>
+      <Flex pt={8}>
+        <Dropzone />
+      </Flex>
+    </Layout>
+  );
 };
 
 export default Home;

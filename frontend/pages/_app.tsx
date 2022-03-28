34a5b9306b1cf4bc77ae9b0d0a427b9d3da314c0
tulakann <tulakan.r@gmail.com>
2022-03-28 17:14:48 +0700

    feat: change font

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,18 @@
-import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "@fontsource/ibm-plex-sans-thai";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/globals.css";
+
+const theme = extendTheme({
+  fonts: {
+    heading: '"IBM Plex Sans Thai", sans-serif',
+    body: '"IBM Plex Sans Thai", sans-serif',
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );

4cf340695c4f5975608e8675de02361a9eabe21b
tulakann <tulakan.r@gmail.com>
2022-04-28 14:17:40 +0700

    fix(assessment): remove unnecessary nested container

--- frontend/pages/assessment.tsx
+++ frontend/pages/assessment.tsx
@@ -96,7 +96,7 @@ const Assessment = () => {
               h="100%"
               backgroundColor="white"
             />
-            <Container maxW="container.lg" position="relative">
+            <Box position="relative">
               <Stack direction="column" gap={4} mt={10} alignItems="flex-start">
                 <Heading as="h4" fontSize="2xl" color="secondary.400" mb={6}>
                   ผลการคำนวณ
@@ -116,81 +116,78 @@ const Assessment = () => {
                 </Box>
               </Box>
             </Box>
+          </Box>
+
           {/* model description */}
           <Box>
-            <Container maxW="container.lg">
-              <VStack gap={6} alignItems="flex-start" py={12} pb={16}>
-                <Heading
-                  as="h5"
-                  fontWeight="semibold"
-                  size="md"
-                  color="secondary.400"
-                >
-                  สูตรการคำนวณความเสี่ยง
+            <VStack gap={6} alignItems="flex-start" py={12} pb={16}>
+              <Heading
+                as="h5"
+                fontWeight="semibold"
+                size="md"
+                color="secondary.400"
+              >
+                สูตรการคำนวณความเสี่ยง
+              </Heading>
+              <VStack gap={2} fontSize="sm" px={6} alignItems="flex-start">
+                <Text>
+                  สูตรการคำนวณความเสี่ยงนี้ถูกสร้างขึ้นมาจากข้อมูลทางคลินิกจากกลุ่มประชากรที่เข้ามาตรวจ
+                  ณ​ ศูนย์โรคหัวใจ โรงพยาบาลศิริราช
+                  โดยใช้ฟังก์ชันการเรียนรู้แบบถอดถอดแบบโลจิสติกส์ (Logistic
+                  regression) เพื่อทำนายความน่าจะเป็นของการมีโรคหัวใจประเภทต่างๆ
+                  โดยสมการที่ใช้คำนวณเป็นไปตามสูตรต่อไปนี้
+                </Text>
+
+                <Heading as="h6" fontWeight="semibold" fontSize="md">
+                  ความน่าจะเป็นของการมีแผลเป็นในกล้ามเนื้อหัวใจ (Myocardial
+                  Infarction, MI)
                 </Heading>
-                <VStack gap={2} fontSize="sm" px={6} alignItems="flex-start">
-                  <Text>
-                    สูตรการคำนวณความเสี่ยงนี้ถูกสร้างขึ้นมาจากข้อมูลทางคลินิกจากกลุ่มประชากรที่เข้ามาตรวจ
-                    ณ​ ศูนย์โรคหัวใจ โรงพยาบาลศิริราช
-                    โดยใช้ฟังก์ชันการเรียนรู้แบบถอดถอดแบบโลจิสติกส์ (Logistic
-                    regression)
-                    เพื่อทำนายความน่าจะเป็นของการมีโรคหัวใจประเภทต่างๆ
-                    โดยสมการที่ใช้คำนวณเป็นไปตามสูตรต่อไปนี้
-                  </Text>
+                <Code {...CodeProps}>
+                  logit_scar = (-1.253690 + (-1.289421 * female) + (1.267990*
+                  history_mi) + (0.852316 * history_ptca_cabg) + (1.188963 *
+                  history_cad) + (0.875971 * history_hf) + (0.394268 *
+                  history_ckd))
+                </Code>
 
-                  <Heading as="h6" fontWeight="semibold" fontSize="md">
-                    ความน่าจะเป็นของการมีแผลเป็นในกล้ามเนื้อหัวใจ (Myocardial
-                    Infarction, MI)
-                  </Heading>
-                  <Code {...CodeProps}>
-                    logit_scar = (-1.253690 + (-1.289421 * female) + (1.267990*
-                    history_mi) + (0.852316 * history_ptca_cabg) + (1.188963 *
-                    history_cad) + (0.875971 * history_hf) + (0.394268 *
-                    history_ckd))
-                  </Code>
+                <Heading as="h6" fontWeight="semibold" fontSize="md">
+                  ความน่าจะเป็นของการเป็นโรคหลอดเลือดหัวใจตีบหรือตัน (Coronary
+                  Artery Disease, CAD)
+                </Heading>
+                <Code {...CodeProps}>
+                  logit_cad_scar = (-1.838088 + (-1.257548 * female) + (0.290813
+                  * diabetes_mellitus) + (1.428430 * history_mi). +(0.915726 *
+                  history_ptca_cabg) + (1.477169 * history_cad) + (0.519559 *
+                  history_hf) + (0.359040 * history_ckd))
+                </Code>
 
-                  <Heading as="h6" fontWeight="semibold" fontSize="md">
-                    ความน่าจะเป็นของการเป็นโรคหลอดเลือดหัวใจตีบหรือตัน (Coronary
-                    Artery Disease, CAD)
-                  </Heading>
-                  <Code {...CodeProps}>
-                    logit_cad_scar = (-1.838088 + (-1.257548 * female) +
-                    (0.290813 * diabetes_mellitus) + (1.428430 * history_mi).
-                    +(0.915726 * history_ptca_cabg) + (1.477169 * history_cad) +
-                    (0.519559 * history_hf) + (0.359040 * history_ckd))
-                  </Code>
+                <Heading as="h6" fontWeight="semibold" fontSize="md">
+                  ความน่าจะเป็นที่ประสิทธิภาพการบีบตัวหัวใจห้องล่างซ้ายตำ่กว่า
+                  40% (LVEF &lt; 40)
+                </Heading>
+                <Code {...CodeProps}>
+                  logit_lvef40 = (-1.986239 + (-0.573404 * age_above_65) +
+                  (-0.537664 * female) + (-0.771137 * bmi_above_25) + (-0.533213
+                  * hypertension) + (0.714925 * history_cad) + (2.065291 *
+                  history_hf) + (0.619290 * history_ckd))
+                </Code>
 
-                  <Heading as="h6" fontWeight="semibold" fontSize="md">
-                    ความน่าจะเป็นที่ประสิทธิภาพการบีบตัวหัวใจห้องล่างซ้ายตำ่กว่า
-                    40% (LVEF &lt; 40)
-                  </Heading>
-                  <Code {...CodeProps}>
-                    logit_lvef40 = (-1.986239 + (-0.573404 * age_above_65) +
-                    (-0.537664 * female) + (-0.771137 * bmi_above_25) +
-                    (-0.533213 * hypertension) + (0.714925 * history_cad) +
-                    (2.065291 * history_hf) + (0.619290 * history_ckd))
-                  </Code>
+                <Heading as="h6" fontWeight="semibold" fontSize="md">
+                  ความน่าจะเป็นที่ประสิทธิภาพการบีบตัวหัวใจห้องล่างซ้ายตำ่กว่า
+                  50% (LVEF &lt; 50)
+                </Heading>
+                <Code {...CodeProps}>
+                  logit_lvef50 = (-1.468317 + (-0.438355 * age_above_65) +
+                  (-0.797398 * female) + (-0.3547618 * bmi_above_25) +
+                  (-0.350557 * hypertension) + (0.534031 * history_mi) +
+                  (0.787748 * history_cad) + (1.831592 * history_hf))
+                </Code>
 
-                  <Heading as="h6" fontWeight="semibold" fontSize="md">
-                    ความน่าจะเป็นที่ประสิทธิภาพการบีบตัวหัวใจห้องล่างซ้ายตำ่กว่า
-                    50% (LVEF &lt; 50)
-                  </Heading>
-                  <Code {...CodeProps}>
-                    logit_lvef50 = (-1.468317 + (-0.438355 * age_above_65) +
-                    (-0.797398 * female) + (-0.3547618 * bmi_above_25) +
-                    (-0.350557 * hypertension) + (0.534031 * history_mi) +
-                    (0.787748 * history_cad) + (1.831592 * history_hf))
-                  </Code>
-
-                  <Heading as="h6" fontWeight="semibold" fontSize="md">
-                    เมื่อได้ Logit มาแล้ว นำไปคำนวณหาความน่าจะเป็นจากสูตร
-                  </Heading>
-                  <Code {...CodeProps}>
-                    probability = 1 / (1 + exp(-logit))
-                  </Code>
-                </VStack>
+                <Heading as="h6" fontWeight="semibold" fontSize="md">
+                  เมื่อได้ Logit มาแล้ว นำไปคำนวณหาความน่าจะเป็นจากสูตร
+                </Heading>
+                <Code {...CodeProps}>probability = 1 / (1 + exp(-logit))</Code>
               </VStack>
-            </Container>
+            </VStack>
           </Box>
         </>
       )}
